0ef10628ed38fff10a0cd76736e04823c2f1d07c
Zi <znlambov@gmail.com>
2017-02-10 18:23:09 +0200

    Add files via upload

--- includes/languages/bulgarian/account_history_info.php
+++ includes/languages/bulgarian/account_history_info.php
@@ -7,34 +7,34 @@
  * @version $Id: account_history_info.php 3027 2006-02-13 17:15:51Z drbyte $
  */
 
-define('NAVBAR_TITLE', 'My Account');
-define('NAVBAR_TITLE_1', 'My Account');
-define('NAVBAR_TITLE_2', 'History');
-define('NAVBAR_TITLE_3', 'Order #%s');
+define('NAVBAR_TITLE', 'Моят профил');
+define('NAVBAR_TITLE_1', 'Моят профил');
+define('NAVBAR_TITLE_2', 'История');
+define('NAVBAR_TITLE_3', 'Поръчка №%s');
 
-define('HEADING_TITLE', 'Order Information');
+define('HEADING_TITLE', 'Информация за поръчката');
 
-define('HEADING_ORDER_NUMBER', 'Order #%s');
-define('HEADING_ORDER_DATE', 'Order Date:');
-define('HEADING_ORDER_TOTAL', 'Order Total:');
+define('HEADING_ORDER_NUMBER', 'Поръчка №%s');
+define('HEADING_ORDER_DATE', 'Дата на поръчката:');
+define('HEADING_ORDER_TOTAL', 'Стойност на поръчката:');
 
-define('HEADING_DELIVERY_ADDRESS', 'Delivery Address');
-define('HEADING_SHIPPING_METHOD', 'Shipping Method');
+define('HEADING_DELIVERY_ADDRESS', 'Адрес за доставка');
+define('HEADING_SHIPPING_METHOD', 'Метод на доставка');
 
-define('HEADING_PRODUCTS', 'Products');
-define('HEADING_TAX', 'Tax');
-define('HEADING_TOTAL', 'Total');
-define('HEADING_QUANTITY', 'Qty.');
+define('HEADING_PRODUCTS', 'Продукти');
+define('HEADING_TAX', 'Данък');
+define('HEADING_TOTAL', 'Тотал');
+define('HEADING_QUANTITY', 'Количество');
 
-define('HEADING_BILLING_ADDRESS', 'Billing Address');
-define('HEADING_PAYMENT_METHOD', 'Payment Method');
+define('HEADING_BILLING_ADDRESS', 'Адрес на платеца');
+define('HEADING_PAYMENT_METHOD', 'Начин на плащане');
 
-define('HEADING_ORDER_HISTORY', 'Status History &amp; Comments');
-define('TEXT_NO_COMMENTS_AVAILABLE', 'No comments available.');
-define('TABLE_HEADING_STATUS_DATE', 'Date');
-define('TABLE_HEADING_STATUS_ORDER_STATUS', 'Order Status');
-define('TABLE_HEADING_STATUS_COMMENTS', 'Comments');
-define('QUANTITY_SUFFIX', '&nbsp;ea.  ');
+define('HEADING_ORDER_HISTORY', 'Статус, история, коментари');
+define('TEXT_NO_COMMENTS_AVAILABLE', 'Няма налични коментари.');
+define('TABLE_HEADING_STATUS_DATE', 'Дата');
+define('TABLE_HEADING_STATUS_ORDER_STATUS', 'Статус на поръчката');
+define('TABLE_HEADING_STATUS_COMMENTS', 'Коментари');
+define('QUANTITY_SUFFIX', '&nbsp;бр.  ');
 define('ORDER_HEADING_DIVIDER', '&nbsp;-&nbsp;');
 define('TEXT_OPTION_DIVIDER', '&nbsp;-&nbsp;');
 ?>
